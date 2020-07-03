5053b0688681917d110a580baafeaf8a0cb1b3ff
harshyadav5 <harsh.yadav@tothenew.com>
2020-07-03 11:19:47 +0530

    npm eject done

--- demo-testing-with-jest-and-enzyme/src/App.js
+++ demo-testing-with-jest-and-enzyme/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Testing Demo
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div data-test="component-app">
+        <h1>App</h1>
+      </div>
+    );
+  } 
 }
 
 export default App;
--- demo-testing-with-jest-and-enzyme/src/App.test.js
+++ demo-testing-with-jest-and-enzyme/src/App.test.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+});
+
+test('renders counter display', () => {
+});
+
+test('renders counter starts at 0', () => {
+});
+
+test('renders clicking button increments counter display', () => {
 });
