84935b9f3278e5a4e8812411a9b3461975f6e374
harshyadav5 <harsh.yadav@tothenew.com>
2020-07-07 13:35:06 +0530

    Test and Code for non zero guessed

---
+++ jotto-app/src/GuessedWords.test.js
@@ -0,0 +1,132 @@
+// import React from 'react';
+// import {shallow} from 'enzyme';
+// import {findByTestAttr,checkProps} from './test/testUtil';
+// import GuessedWords from './GuessedWords';
+
+// const defaultProps = {
+//     guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+//   };
+// /**
+//  * Factory function to create a ShallowWrapper for the congrts component
+//  * @function setUp
+//  * @param {*} props - Component props specific to this setup
+//  * @returns {ShallowWrapper}
+//  */
+// const setUp = (props = {}) =>{
+//     const setUpProps = {...defaultProps, ...props};
+//     return shallow(<GuessedWords {...setUpProps} />)
+// }
+
+// test('does not throw warning with excepted props', () => {
+//     checkProps(GuessedWords,defaultProps);
+// }) 
+
+// describe('if there are no words guessed', () =>{
+//     let wrapper;
+//     beforeEach(() => {
+//         wrapper = setUp({guessedWords: []});
+//     });
+//     test('renders without error', () => {
+//         const component = findByTestAttr(wrapper,'component-guessed-words');
+//         expect(component.length).toBe(1); 
+//     })
+//     test('renders instructions to guess a word', () => {
+//         const instructions = findByTestAttr(wrapper,'guessed-instructions');
+//         expect(instructions.text().length).not.toBe(0);
+//     })
+// })
+// describe('if there are words guessed', () => {
+//     let wrapper;
+//     const guessedWords = [
+//         {guessedWord: 'train', letterMatchCount: 3},
+//         {guessedWord: 'agile', letterMatchCount: 1},
+//         {guessedWord: 'party', letterMatchCount: 5}
+//     ];
+//     beforeEach(() => {
+//         wrapper = setUp({guessedWords});
+//     })
+//     test('renders without error', () => {
+//         const component = findByTestAttr(wrapper,'component-guessed-words');
+//         expect(component.length).toBe(1); 
+//     })
+//     test('renders "guessed words" section', () => {
+//         const guessedWordNode = findByTestAttr(wrapper, 'guessed-words');
+//         console.log(guessedWordNode)
+//         expect(guessedWordNode.length).toBe(1);
+//     })
+//     test('correct number of guessed words', () => {
+//         const guessedWordNode = findByTestAttr(wrapper, 'guessed-word');
+//         expect(guessedWordNode.length).toBe(guessedWords.length);
+//     })
+// })
+
+// describe('if there are words guessed', () => {
+
+// })
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from './test/testUtil';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+};
+
+/**
+ * Factory function to create a ShallowWrapper for GuessedWords component.
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test('renders without error', () => {
+    wrapper = setup({ guessedWords: [] });
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 }
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
+});
